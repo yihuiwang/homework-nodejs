98f854f1e6bde74f9697befd80ab2f351adcb0fd
Yihui Wang <yihuisend@gmail.com>
2022-10-23 10:47:07 -0700

    Upload U2hw5-YW

--- javascript/U2/hw5.js
+++ javascript/U2/hw5.js
@@ -1,39 +1,58 @@
-function recursion(n, count = 0, process = []) {
-    if (n === 1) {
-        return { count, process };
+var elements = ['Spades','Hearts','Diamonds','Clubs']
+var num = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+
+console.log('The format of the 1st print out (13 cards per line):')
+var spades = createCards(elements.slice(0,1),num)
+var hearts = createCards(elements.slice(1,2),num)
+var diamonds = createCards(elements.slice(2,3),num)
+var clubs = createCards(elements.slice(3,4),num)
+printCards(spades)
+printCards(hearts)
+printCards(diamonds)
+printCards(clubs)
+
+
+console.log('The format of the 2nd print out (13 cards per line):')
+shuffle(spades)
+shuffle(hearts)
+shuffle(diamonds)
+shuffle(clubs)
+printCards(spades)
+printCards(hearts)
+printCards(diamonds)
+printCards(clubs)
+
+console.log('The format of the 3rd print out (13 cards per line):')
+var allcards = createCards(elements,num)
+shuffle(allcards)
+printCards(allcards.slice(0,13))
+printCards(allcards.slice(13,26))
+printCards(allcards.slice(26,39))
+printCards(allcards.slice(39,52))
+
+
+function createCards(elements, num){
+    var cards =[]
+    for(let j = 0; j < elements.length; j++){
+        for(let i = 0; i < num.length; i++){
+            cards.push((num[i] +' of '+ elements[j]).toString()) 
+        }
     }
-    if (n % 2 === 0) {
-        return recursion(n / 2, count + 1, [...process, n / 2]);
-    } else {
-        return recursion(n * 3 + 1, count + 1, [...process, n * 3 + 1]);
+    return cards;
+}
+
+function printCards(cards){
+    var str = cards.join(',')+'.';
+    console.log(str)
+}
+
+function shuffle(arr){
+    let currentIndex = arr.length, randomIndex;
+    while(currentIndex != 0){
+        randomIndex = Math.floor(Math.random()*currentIndex)
+        currentIndex--;
+        [arr[currentIndex],arr[randomIndex]] = [arr[randomIndex],arr[currentIndex]] 
     }
+    return arr;
 }
 
-console.log(recursion(3));
-
-
-const obj = {name: 'test'};
-obj.name = 'hello'
-
-var x;
-function fn;
-...
-console.log(fn());
-x = 0;
-
-
-function fn() {
-    ...
-}
-
-var fn = function () { // () => {}
-
-}
-
-fn()
-
-obj1 = {id: 1}
-// obj2 = obj1;
-// obj.id = 2;
-obj2 = {id: 1}
-obj1.id = 2
